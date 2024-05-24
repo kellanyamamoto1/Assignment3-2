3ab5f51a11d2e71ded9379a69450b49aa59b4664
kellany <kellany@uci.edu>
2024-05-23 17:27:55 -0700

    unititest create_file function working

--- test_file.py
+++ test_file.py
@@ -4,6 +4,7 @@
 
 import unittest
 from unittest.mock import patch, mock_open, MagicMock
+from pathlib import Path
 import ui
 
 class TestOpenFile(unittest.TestCase):
@@ -44,6 +45,24 @@ class TestOpenFile(unittest.TestCase):
         mock_open.assert_called_once_with('/path/to/userfile.dsu', 'r+')
         self.assertEqual(result, '/path/to/userfile.dsu')
         mock_commands.assert_called_once()
+
+class TestCreateFile(unittest.TestCase):
+    @patch('builtins.input', side_effect=["testuser", "testpassword", "testbio"])
+    @patch('ui.commands')
+    @patch('ui.Profile.save_profile')
+    @patch('ui.open', new_callable=mock_open)
+    def test_create_file_as_administrator(self, mock_open, mock_save_profile, mock_commands, mock_input):
+        ui.ADMINISTRATOR = True
+        user_input = ["create", "/path/to", "-n", "testfile"]
+
+        result = ui.create_file(user_input)
+
+        expected_path = Path("/path/to/testfile.dsu")
+        mock_open.assert_any_call(expected_path, 'a')
+        self.assertEqual(mock_open.call_count, 2)
+        mock_save_profile.assert_called_once_with(path=expected_path)
+        self.assertEqual(result, expected_path)
+
 
 
 if __name__ == '__main__':
